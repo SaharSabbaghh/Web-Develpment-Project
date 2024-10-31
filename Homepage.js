5082de74ad72c096a13df13afc02c2eed65d2b04
fatimataha22 <fatima.taha04@gmail.com>
2024-10-31 13:26:08 +0200

    Added the welcome message div in the home page

--- Homepage.js
+++ Homepage.js
@@ -1,3 +1,4 @@
+// NavBar
 let activePage = window.location.pathname.split('/').pop(); 
 document.querySelectorAll('ul.ul1 li a').forEach(link => {
     let linkPage = link.href.split('/').pop(); 
@@ -6,6 +7,7 @@ document.querySelectorAll('ul.ul1 li a').forEach(link => {
     }
 });
 
+// SideBar
 document.querySelector(".ham-icon i").addEventListener("click", function(){
     document.querySelector(".sidebar").classList.remove("hidden");
 })
@@ -28,4 +30,31 @@ document.querySelector(".subscribe-btn button").addEventListener("click", functi
         errorMessage.style.display = "none";
         emailInput.value = "";
     }
+});
+
+// Home Image
+const imageContainer = document.querySelector(".home-img");
+const hoverImage = document.querySelector(".home-img img");
+function moveInDirection(event, element) {
+  const { offsetX, offsetY, target } = event;
+  const width = target.offsetWidth;
+  const height = target.offsetHeight;
+  const moveDistance = 15; 
+  if (offsetX < width / 3) {
+    element.style.transform = `translateX(-${moveDistance}px)`;
+  } else if (offsetX > (2 * width) / 3) {
+    element.style.transform = `translateX(${moveDistance}px)`;
+  } else if (offsetY < height / 3) {
+    element.style.transform = `translateY(-${moveDistance}px)`;
+  } else if (offsetY > (2 * height) / 3) {
+    element.style.transform = `translateY(${moveDistance}px)`;
+  } else {
+    element.style.transform = 'scale(1.02)'; 
+  }
+}
+imageContainer.addEventListener('mousemove', (event) => {
+  moveInDirection(event, hoverImage); 
+});
+imageContainer.addEventListener('mouseleave', () => {
+  hoverImage.style.transform = 'translate(0, 0)'; 
 });
