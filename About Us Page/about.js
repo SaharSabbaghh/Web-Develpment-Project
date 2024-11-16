536d57fa4796feb7e11c2d60a2c73cc7497a4da9
fatimataha22 <fatima.taha04@gmail.com>
2024-11-16 16:20:08 +0200

    Fixed all header, footer and their linking

--- About Us Page/about.js
+++ About Us Page/about.js
@@ -1,17 +1,3 @@
-$(document).ready(function () {
-    $(".filter-item").on("click", function () {
-        const value = $(this).data("filter");
-
-        if (value === "all") {
-            $(".post-box").show(600); 
-        } else {
-            $(".post-box").hide(600).filter(`.${value}`).show(600);
-        }
-
-        $(this).addClass("active-filter").siblings().removeClass("active-filter");
-    });
-});
-
 // NavBar
 let activePage = window.location.pathname.split('/').pop(); 
 document.querySelectorAll('ul.ul1 li a').forEach(link => {
